e64b5f6b3824863194cdad7e1384a371cdf213eb
Olatunji Oduola <ola.oduola@gmail.com>
2020-07-14 12:32:36 +0100

    Refactored test using before block to create new account for each test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,23 +1,26 @@
 require 'account'
 
 RSpec.describe Account do
+
+  before(:each) do
+    @account_one = Account.new
+  end
  
   context '#balance' do
     it 'returns 10 deposited into account' do
-      subject.deposit(10)
-      expect(subject.balance).to eq 10
+      @account_one.deposit(10)
+      expect(@account_one.balance).to eq 10
     end
 
     it 'returns 20 deposited into account' do
-      subject.deposit(20)
-      expect(subject.balance).to eq 20
+      @account_one.deposit(20)
+      expect(@account_one.balance).to eq 20
     end
 
     it 'returns 30 deposited over 2 transactions' do
-      account_one = Account.new
-      account_one.deposit(20)
-      account_one.deposit(10)
-      expect(account_one.balance).to eq 30
+      @account_one.deposit(20)
+      @account_one.deposit(10)
+      expect(@account_one.balance).to eq 30
     end
   end
 end
