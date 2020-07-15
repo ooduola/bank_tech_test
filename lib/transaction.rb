4799196ba65221be236d83b66e93ec28766ba227
Olatunji Oduola <ola.oduola@gmail.com>
2020-07-15 16:14:56 +0100

    Test passed. Transaction object now returns balance value when initalized with balance value

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,12 +1,13 @@
 class Transaction
 
-  def initialize(credit: nil, debit: nil) 
+  def initialize(credit: nil, debit: nil, balance: nil) 
     @debit = debit
     @credit = credit
+    @balance = balance
   end
 
   def display
-    "01/07/2020||#{@credit} ||#{@debit} || "
+    "01/07/2020||#{@credit} ||#{@debit} ||#{@balance} "
   end
 
 
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,21 +2,26 @@ require 'transaction'
 
 RSpec.describe Transaction do
 
-  date = Time.new(2020, 07, 01, 12)
+  @date = "01/07/2020"
 
   it 'creates a new date when initalised' do
     # allow(Time).to receive(:now).and_return(date) 
-    expect(subject.display).to eq '01/07/2020|| || || '
+    expect(subject.display).to eq "01/07/2020|| || || "
   end
 
-  it 'returns credit amount when initialized with credit argument' do
+  it 'returns credit value when initialized with credit argument' do
     subject = described_class.new(credit: 1000)
     expect(subject.display).to eq '01/07/2020||1000 || || '
   end
 
-  it 'returns debit amount when initialized with debit argument' do
+  it 'returns debit value when initialized with debit argument' do
     subject = described_class.new(debit: 500)
     expect(subject.display).to eq '01/07/2020|| ||500 || '
+  end
+
+  it "returns balance value when initialized with balance argument" do
+    subject = described_class.new(balance: 1500) 
+    expect(subject.display).to eq '01/07/2020|| || ||1500 '
   end
 
 end
