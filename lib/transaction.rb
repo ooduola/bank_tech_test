2c0012110424ad6bcfa3dd715ab7ee76f21678ad
Olatunji Oduola <ola.oduola@gmail.com>
2020-07-15 15:09:09 +0100

    Test passed. Display now returns credit amount if initialized with a credit value

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,10 +1,12 @@
 class Transaction
 
-  def initialize
+  def initialize(credit: nil) 
+
+    @credit = credit
   end
 
   def display
-    '01/07/2020 || || || '
+    "01/07/2020||#{@credit} || || "
   end
 
 
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -6,6 +6,12 @@ RSpec.describe Transaction do
 
   it 'creates a new date when initalised' do
     # allow(Time).to receive(:now).and_return(date) 
-    expect(subject.display).to eq '01/07/2020 || || || '
+    expect(subject.display).to eq '01/07/2020|| || || '
   end
+
+  it 'returns credit amount when initialized with credit argument' do
+    subject = described_class.new(credit: 1000)
+    expect(subject.display).to eq '01/07/2020||1000 || || '
+  end
+
 end
